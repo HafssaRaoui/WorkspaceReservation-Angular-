5fb0f700f0f551bb8f99913b21d2f68d831011e9
Mariam LAKHSASSI <mariamlakhsassi@gmail.com>
2024-07-30 14:17:37 +0100

    date picker match the url

--- src/app/shared-data.service.ts
+++ src/app/shared-data.service.ts
@@ -7,8 +7,6 @@ import { BehaviorSubject, ReplaySubject } from 'rxjs';
 export class SharedDataService {
   private selectedDateSubject = new ReplaySubject<Date>(1);
   currentSelectedDate = this.selectedDateSubject.asObservable();
-
-
   updateSelectedDate(date: Date) {
     this.selectedDateSubject.next(date);
   }
